033ca55cc7cb8a7b42c6e79c304b09776279825f
mmix-el <mmix-el@yandex.ru>
2021-08-24 13:36:21 +0500

    Init commit

---
+++ app/src/main/java/xyz/mmixel/tipcalculatorapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.mmixel.tipcalculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
